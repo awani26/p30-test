ddfc844097f79fb7dc7d9e25af6f252044ca2ec4
awani26 <76963367+awani26@users.noreply.github.com>
2021-04-21 18:05:09 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block {
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      //this.image= loadImage("sprites/block.png");
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+
+      this.Visibility = 255
+    }
+    display(){
+      
+      //console.log(this.body.speed);
+    if(this.body.speed < 3){
+     
+    }
+    else{
+      World.remove(world,this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+      tint(255,this.Visibility);
+      //image(this.image, this.body.position.x,this.body.position.y,50,50)
+      pop();
+      var pos= this.body.position;
+      rectMode(CENTER);
+     rect(pos.x,pos.y,this.width, this.height);
+
+    }
+}
+
+}
